b6dc19a9b5cb00653c7307d651ba2d08cb6ada7b
Daniel Medrado <d_medrado@yahoo.com>
2024-04-24 16:53:11 -0300

    Delete database.js

--- database.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require(mongoose)
-async function conexao(){
-    try{
-        await mongoose.connect("mongodb+srv://alecampos1976:ZUq0lulWM8CRu3s0@cluster0.wohbmou.mongodb.net/")
-    }catch(error){
-console.log("Erro de conexão com o banco de dados:", error)
-    }
-}
